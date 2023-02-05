012d2ab94495209260c51073abf2d6d344649cf0
Tiisetso Dinoko <dinokovonteese@gmail.com>
2023-02-05 15:24:37 +0200

    Updated padding for the bottom button with responsive values

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -3,84 +3,85 @@ import 'package:flutter/material.dart';
 import 'package:money_money_money/components/money_card.dart';
 import 'package:money_money_money/constants.dart';
 import 'package:money_money_money/components/bottom_button.dart';
+import 'package:money_money_money/utils/dimensions.dart';
 
 import 'home_page.dart';
 
-
-
 class ResultsPage extends StatelessWidget {
-
   final String package;
   final String feedback;
 
-  const ResultsPage({Key? key, required this.package, required this.feedback}) : super(key: key);
+  const ResultsPage({Key? key, required this.package, required this.feedback})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black.withOpacity(0.70),
-          title: const Text(
-            'IT\'S MONEY',
-            style: TextStyle(
-              letterSpacing: 1.5,
-              color: moneyColor,
-              fontFamily: 'MinecraftTen',
-              fontSize: 30,
-            ),
+    Dimensions.init(context: context); //instantiate the context here
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black.withOpacity(0.70),
+        title: const Text(
+          'IT\'S MONEY',
+          style: TextStyle(
+            letterSpacing: 1.5,
+            color: moneyColor,
+            fontFamily: 'MinecraftTen',
+            fontSize: 30,
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(33.0),
-                alignment: Alignment.bottomLeft,
-                child: const Text(
-                  'YOUR RESULTS',
-                  style: titleTextStyle,
-                ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(33.0),
+              alignment: Alignment.bottomLeft,
+              child: const Text(
+                'YOUR RESULTS',
+                style: titleTextStyle,
               ),
             ),
-            Expanded(
-              flex: 5,
-              child: MoneyCard(
-                color: Colors.black,
-                childCard: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-
-
-                    const Text(
-                        'CONGRATULATIONS',
-                        style: resultTextStyle,
-                      ),
-
-                    BorderedText(
-                      strokeColor: Colors.white,
-                      strokeWidth: 1.4,
-                      child: Text(
-                        package + "  LPA",
-                        style: packageTextStyle,
-                      ),
+          ),
+          Expanded(
+            flex: 4,
+            child: MoneyCard(
+              color: Colors.black,
+              childCard: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'CONGRATULATIONS',
+                    style: resultTextStyle,
+                  ),
+                  BorderedText(
+                    strokeColor: Colors.white,
+                    strokeWidth: 1.4,
+                    child: Text(
+                      package + "  LPA",
+                      style: packageTextStyle,
                     ),
-                    BorderedText(
-                      strokeColor: moneyColor,
-                      strokeWidth: 1,
-                      child: Text(
-                        feedback,
-                        style: resultBodyTextStyle.copyWith(letterSpacing: 1.5, color: Colors.white),
-                        textAlign: TextAlign.center,
-                      ),
+                  ),
+                  BorderedText(
+                    strokeColor: moneyColor,
+                    strokeWidth: 1,
+                    child: Text(
+                      feedback,
+                      style: resultBodyTextStyle.copyWith(
+                          letterSpacing: 1.5, color: Colors.white),
+                      textAlign: TextAlign.center,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            BottomButton(
+          ),
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: Dimensions.screenWidth / 5.5),
+            child: BottomButton(
               title: 'RE-CALCULATE',
               onTap: () {
                 Navigator.pushReplacement(
@@ -89,13 +90,13 @@ class ResultsPage extends StatelessWidget {
                     builder: (context) => const HomePage(),
                   ),
                 );
-                cpCardPressed=false;
-                webdCardPressed=false;
-
+                cpCardPressed = false;
+                webdCardPressed = false;
               },
             ),
-          ],
-        ),
-      );
-    }
+          ),
+        ],
+      ),
+    );
   }
+}
