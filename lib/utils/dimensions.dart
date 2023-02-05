50fdcdcaa7b5a48d823fdf49729bcf75c27819cc
Tiisetso Dinoko <dinokovonteese@gmail.com>
2023-02-05 15:22:50 +0200

    Added new utilities folder to record the dimensions variables and other utilites

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class Dimensions {
+  //init method is static so no object creation is required
+  static void init({
+    required context,
+  }) {
+    //instantiate variables here
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+  }
+
+  //declare variables here
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+}
